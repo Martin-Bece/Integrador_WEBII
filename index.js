277d966074f561a1ebc5f8d6218a459d619b7a08
Martin-Bece <martinbecerrasl7@gmail.com>
2025-05-18 18:34:53 -0300

    Estilos y vista para ver las internaciones

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ app.get('/BuscarPaciente', (req, res) =>{
     res.render('BuscarPaciente')
 })
 
+app.get('/FormularioPaciente', (req, res) =>{
+    res.render('FormularioPaciente')
+})
+
+app.get('/PacientesInternados', (req, res) =>{
+    res.render('PacientesInternados')
+})
+
 app.use((req, res) => {
   res.status(404).render('404');
 });
