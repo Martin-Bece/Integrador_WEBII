1dee5c0ac69a4e15454c0f708dda5ed8e651d8ab
Martin-Bece <martinbecerrasl7@gmail.com>
2025-05-22 16:18:50 -0300

    Vistas finales del proyecto CSS vanila y Bootstrap

--- index.js
+++ index.js
@@ -27,39 +27,59 @@ app.get('/PacientesInternados', (req, res) =>{
 app.get('/portalPaciente', (req, res) =>{
     res.render('portalPaciente', {
   paciente: {
-    id: 1,
+    idPaciente: 1,
     nombre: 'Juan',
     apellido: 'Pérez',
     dni: '12345678',
-    fechaNacimiento: '1985-06-01',
+    fecha_nacimiento: '1985-06-01',
     sexo: 'Masculino',
     telefono: '123456789',
     direccion: 'Calle Falsa 123',
-    contactoEmergencia: 'Ana Pérez',
-    origen: 'Guardia',
-    seguroNombre: 'OSDE',
-    seguroNumero: '998877',
-    motivo: 'Dolor abdominal'
+    mutual: 'OSDE',
+    
   },
   turnos: [
     {
       id: 1,
       fecha: '2025-05-24',
       hora: '10:30',
-      especialidad: 'Clínica Médica',
-      profesional: 'Dr. López',
+      idEspecialidad: 'Clínica Médica',
+      idMedico: 'Dr. López',
       estado: 'Pendiente'
     },
     {
       id: 2,
       fecha: '2025-05-28',
       hora: '14:00',
-      especialidad: 'Cardiología',
-      profesional: 'Dra. Torres',
+      idEspecialidad: 'Cardiología',
+      idMedico: 'Dra. Torres',
       estado: 'Pendiente'
     }
   ]
 });
+})
+
+app.get('/formAdmision', (req, res) =>{
+    const motivos = [
+  { id: 1, nombre: 'Observación clínica' },
+  { id: 2, nombre: 'Cirugía programada' },
+  { id: 3, nombre: 'Tratamiento intensivo' },
+  { id: 4, nombre: 'Control postoperatorio' },
+  { id: 5, nombre: 'Descompensación crónica' }
+];
+
+const origenes = [
+  { id: 1, nombre: 'Guardia' },
+  { id: 2, nombre: 'Derivado de consultorio' },
+  { id: 3, nombre: 'Derivado de otro hospital' },
+  { id: 4, nombre: 'Internación domiciliaria' },
+  { id: 5, nombre: 'Sala de espera' }
+];
+    res.render('formAdmision', { motivos, origenes });
+})
+
+app.get('/Emergencia', (req, res) =>{
+    res.render('formEmergencia')
 })
 
 app.use((req, res) => {
