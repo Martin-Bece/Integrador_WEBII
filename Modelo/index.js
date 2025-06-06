3bbe0e4ffe5d182b217de7f2d0f4ae3f7a1d8835
Martin-Bece <martinbecerrasl7@gmail.com>
2025-06-06 15:33:45 -0300

    Usar DATABASE_URL en Railway

--- Modelo/index.js
+++ Modelo/index.js
@@ -1,17 +1,21 @@
 const Sequelize = require('sequelize');
 const initModels = require('./init-models');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || 'bd_his',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
-  {
-    host: process.env.DB_HOST || 'db',
-    port: 3306, // <- Asegúrate que aquí está el puerto correcto
-    dialect: 'mysql',
-    logging: false
-  }
-);
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'mysql',
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        logging: false,
+      }
+    );
 
 
 const models = initModels(sequelize);
