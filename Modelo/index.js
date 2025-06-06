d70a090ef958bacd474aa84ce2446a60e37c15b4
Martin-Bece <martinbecerrasl7@gmail.com>
2025-06-06 15:18:50 -0300

    mas confg

--- Modelo/index.js
+++ Modelo/index.js
@@ -7,10 +7,12 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || '',
   {
     host: process.env.DB_HOST || 'db',
+    port: 3306, // <- Asegúrate que aquí está el puerto correcto
     dialect: 'mysql',
     logging: false
   }
 );
+
 
 const models = initModels(sequelize);
 
